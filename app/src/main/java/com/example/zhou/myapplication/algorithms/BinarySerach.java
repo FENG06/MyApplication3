163e4c776b4f2428fd67e824a7104f2b7b8255f8
Kevin <sevenny@foxmail.com>
2018-04-02 22:08:01 +0800

    二分法算法

--- app/src/main/java/com/example/zhou/myapplication/algorithms/BinarySerach.java
+++ app/src/main/java/com/example/zhou/myapplication/algorithms/BinarySerach.java
@@ -40,64 +40,11 @@ public class BinarySerach {
     }
 
     public static void main(String[] args) {
-//        int a = Integer.parseInt(args[0]);
-//        int b = Integer.parseInt(args[1]);
-//        int c = Integer.parseInt(args[2]);
-//        judge(a, b, c);
-
 
         System.out.println('b');
         System.out.println('b' + 'c');
         System.out.println((char) ('b' + 4));
         System.out.println(1 + 2 + "k" + 1 + 2 + "k");
 
-
-//        int[] list = {1, 2, 3, 4, 5, 6};
-//        int target = 3;
-//        System.out.println(rank(target, list));
-//
-//
-//        int a = 14;
-//        int b = 3;
-//        int c = -14;
-//        int d = -3;
-//        double e = 14.1;
-//
-//
-//        System.out.println((1 + 2.236) / 2);
-//
-//        System.out.println(1 + 2 + 3 + 4.0);
-//
-//        System.out.println(4.1 >= 4);
-//
-//        System.out.println(1 + 2 + "3");
-
-
-//
-//        int[] whitelist = In.readInts(args[0]);
-//        Arrays.sort(whitelist);
-//        while (!StdIn.isEmpty()) {
-//            int key = StdIn.readInt();
-//            if (rank(key, whitelist) < 0) {
-//                StdOut.println(key);
-//            }
-//        }
-
-//
-//        int N = 100;
-//        StdDraw.setXscale(0, N);
-//        StdDraw.setYscale(0, N * N);
-//        StdDraw.setPenRadius(.01);
-//
-//        for (int i = 0; i <= N; i++) {
-//            StdDraw.point(i, i);
-//            StdDraw.point(i, i * i);
-//            StdDraw.point(i, i * Math.log(i));
-//
-//        }
-
-
     }
-
-
 }
